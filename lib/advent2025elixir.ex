9ae23c35641e6b38bffb1129869d9d818ce7d849
Geoffrey White <geoffpiercewhite@gmail.com>
2025-12-01 11:06:45 -0500

    initial commit and day 1

---
+++ lib/advent2025elixir.ex
@@ -0,0 +1,154 @@
+defmodule Advent2025elixir do
+  @moduledoc """
+  Documentation for `Advent2025elixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Advent2025elixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def advent1 do
+    case File.read("inputs/advent1.txt") do
+      # case File.read("inputs/advent1test.txt") do
+      {:ok, content} ->
+        content = String.replace(content, "\r", "")
+        lines = String.split(content, "\n")
+        {start, next} = Enum.split(lines, 1)
+        line = Enum.at(start, 0)
+        IO.puts(next(line, next, 0, 50, 1))
+    end
+  end
+
+  def next("L" <> rest, [], zero_count, cur_sum, line_num) do
+    restNum = Integer.parse(rest, 10)
+
+    case restNum do
+      :error ->
+        exit("bad")
+
+      {number, ""} ->
+        zeroes_to_add = cur_sum - number
+        IO.puts(zeroes_to_add)
+        div = abs(Integer.floor_div(zeroes_to_add, 100))
+        zero_count = zero_count + div - to_int(cur_sum == 0 && div > 0)
+        modNum = Integer.mod(cur_sum - number, 100)
+        num = Integer.mod(max(modNum, 100 - modNum), 100)
+
+        if num == 0 do
+          zero_count + 1
+        else
+          zero_count
+        end
+    end
+  end
+
+  def next("L" <> rest, [next_line | next_lines], zero_count, cur_sum, line_num) do
+    restNum = Integer.parse(rest, 10)
+
+    case restNum do
+      :error ->
+        exit("bad")
+
+      {number, ""} ->
+        zeroes_to_add = cur_sum - number
+
+        zc = zero_count
+        div = abs(Integer.floor_div(zeroes_to_add, 100))
+        zc = zc + div
+        zc = zc - to_int(cur_sum == 0 && div > 0)
+        modNum = Integer.mod(cur_sum - number, 100)
+        num = modNum
+
+        if num < 0 do
+          num = 100 - modNum
+          next(next_line, next_lines, zc, num, line_num + 1)
+        end
+
+        IO.puts(Integer.to_string(cur_sum) <> "-" <> rest <> "=" <> Integer.to_string(num))
+        IO.puts(zeroes_to_add)
+        IO.puts(zc)
+
+        if num == 0 do
+          next(next_line, next_lines, zc + 1, num, line_num + 1)
+        else
+          next(next_line, next_lines, zc, num, line_num + 1)
+        end
+    end
+  end
+
+  def to_int(bool) do
+    if bool, do: 1, else: 0
+  end
+
+  def next("R" <> rest, [], zero_count, cur_sum, line_num) do
+    restNum = Integer.parse(rest, 10)
+
+    case restNum do
+      :error ->
+        exit("bad")
+
+      {number, ""} ->
+        zeroes_to_add = cur_sum + number
+
+        zero_count =
+          zero_count + abs(Integer.floor_div(zeroes_to_add, 100)) - to_int(zeroes_to_add == 100) -
+            to_int(
+              cur_sum == 0 && abs(Integer.floor_div(zeroes_to_add, 100)) > 0 &&
+                zeroes_to_add != 100
+            )
+
+        num = Integer.mod(cur_sum + number, 100)
+
+        if num == 0 do
+          zero_count + 1
+        else
+          zero_count
+        end
+    end
+  end
+
+  def next("R" <> rest, [next_line | next_lines], zero_count, cur_sum, line_num) do
+    restNum = Integer.parse(rest, 10)
+
+    case restNum do
+      :error ->
+        exit("bad")
+
+      {number, ""} ->
+        zeroes_to_add = cur_sum + number
+
+        zero_count =
+          zero_count + abs(Integer.floor_div(zeroes_to_add, 100))
+          # - to_int(zeroes_to_add == 100) -
+          #   to_int(
+          #     cur_sum == 0 && abs(Integer.floor_div(zeroes_to_add, 100)) > 0 &&
+          #       zeroes_to_add != 100
+          #   )
+
+        num = Integer.mod(cur_sum + number, 100)
+        IO.puts(Integer.to_string(cur_sum) <> "+" <> rest <> "=" <> Integer.to_string(num))
+        IO.puts(Integer.to_string(zero_count))
+        IO.puts(Integer.to_string(abs(Integer.floor_div(zeroes_to_add, 100))))
+
+          next(next_line, next_lines, zero_count, num, line_num + 1)
+        # if num == 0 do
+        #   next(next_line, next_lines, zero_count + 1, num, line_num + 1)
+        # else
+        #   next(next_line, next_lines, zero_count, num, line_num + 1)
+        # end
+    end
+  end
+
+  def next("", [], zero_count, cur_sum, line_num) do
+    zero_count
+  end
+end
---
+++ test/advent2025elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule Advent2025elixirTest do
+  use ExUnit.Case
+  doctest Advent2025elixir
+
+  test "greets the world" do
+    assert Advent2025elixir.hello() == :world
+  end
+end
